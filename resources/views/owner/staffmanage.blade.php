156aa089a5ce834a5608ba5c78f0741e35b49588
Ngo Thi Kim Oanh <ntkoanh.orin@outlook.com>
2023-12-23 11:50:45 +0700

    đổi tên

--- resources/views/owner/staffmanage.blade.php
+++ resources/views/owner/staffmanage.blade.php
@@ -31,7 +31,7 @@ Owner page
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Thêm người dùng mới</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Thêm nhân viên mới</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form method="POST">
@@ -52,7 +52,7 @@ Owner page
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
-                    <button type="submit" class="btn btn-primary">Thêm người dùng mới</button>
+                    <button type="submit" class="btn btn-primary">Thêm nhân viên mới</button>
                 </div>
             </form>
         </div>
