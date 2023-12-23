7820849dad0ef5a4d0ed5912f4e6b1e3e4543940
Ngo Thi Kim Oanh <ntkoanh.orin@outlook.com>
2023-12-23 18:59:27 +0700

    keep_trang_order

--- resources/views/staff/orders.blade.php
+++ resources/views/staff/orders.blade.php
@@ -5,13 +5,12 @@ Owner page
 @endsection
 
 @section('body')
-
-@foreach($orders as $order_id => $detail)
 <div class="container bg-body ps-5 pe-5 pb-4">
     <a class="btn btn-secondary float-end mt-4 mb-4 me-4" href="/staff/order">
         <i class="fa-solid fa-backward"></i>Trang order
     </a>
 </div>
+@foreach($orders as $order_id => $detail)
 <div class="container bg-body border rounded mt-4 mb-4 shadow-sm p-4 d-flex">
     <div class="flex-grow-1">
         <h6>{{ $detail['created_at'] }}</h6>
